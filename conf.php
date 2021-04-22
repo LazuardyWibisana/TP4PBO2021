831c11135e50e6545aaee909c828f7bb6f99e4c7
ardy505 <75084894+ardy505@users.noreply.github.com>
2021-04-22 22:33:06 +0700

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,14 @@
+<?php
+
+/******************************************
+PRAKTIKUM RPL
+ ******************************************/
+
+// Konfigurasi basis data
+
+$db_host = "128.0.0.1"; // host 
+$db_user = "root"; // user
+$db_password = ""; // password
+$db_name = "db_task"; // nama basis data
+
+ ?>
